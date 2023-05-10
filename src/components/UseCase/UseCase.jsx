45aedfb03f15a86fdaf4ebaa658741b507ff0a99
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-05-11 04:35:41 +0530

    new

--- src/components/UseCase/UseCase.jsx
+++ src/components/UseCase/UseCase.jsx
@@ -6,6 +6,11 @@ import Image_3 from "../../assets/usecase/img-features-skinny-3.svg";
 import Image_4 from "../../assets/usecase/img-features-skinny-4.svg";
 import Image_5 from "../../assets/usecase/img-features-skinny-5.svg";
 import Image_6 from "../../assets/usecase/img-features-skinny-6.svg";
+import invest_1 from '../../assets/invester/image-1.svg'
+import invest_2 from '../../assets/invester/image-3.svg'
+import invest_3 from '../../assets/invester/image-4.svg'
+import invest_4 from '../../assets/invester/new_order_logo-1-1.svg'
+import invest_5 from '../../assets/invester/outlierventureslogo-dark.svg'
 
 function UseCase() {
   return (
@@ -43,6 +48,24 @@ function UseCase() {
             <h2>DEX</h2>
           </div>
         </div>
+        
+      </div>
+
+      <div className="invest-head usecase-head text-center">
+             <h1>Investors</h1>
+
+        </div>
+      <div className="Investors">
+      
+        <div className="Investors-grid">
+            <img src={invest_1} alt="" />
+            <img src={invest_2} alt="" />
+            <img src={invest_3} alt="" />
+            <img src={invest_4} alt="" />
+            <img src={invest_5} alt="" />
+
+        </div>
+
       </div>
     </section>
   );
